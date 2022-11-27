2627074624ffea7bfa364783fe5b12f1ef40e22b
kchnms <pashafizmat1@gmail.com>
2022-11-27 17:00:10 +0300

    Добавьте файлы проекта.

---
+++ WrapperEngine.cpp
@@ -0,0 +1,12 @@
+﻿// WrapperEngine.cpp: определяет точку входа для приложения.
+//
+
+#include "WrapperEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
